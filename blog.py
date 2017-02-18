67db68177448c56903ff59fe4ea6c3e04f6980a9
Anand Chitipothu <anandology@gmail.com>
2017-02-18 11:57:51 +0530

    Added newpost

--- blog.py
+++ blog.py
@@ -4,7 +4,8 @@ import db
 
 urls = (
    "/", "index",
-   "/(.*)", "post"
+   "/new", "newpost",
+   "/(.*)", "post",
 )
 render = web.template.render("templates")
 
@@ -19,6 +20,22 @@ class post:
     def GET(self, slug):
         p = db.get_post(slug)
         return render.site(render.post(p))
+
+class newpost:
+    def GET(self):
+        p = web.storage(slug="", title="", body="")
+        return render.site(render.edit(p))
+
+    def POST(self):
+        i = web.input(title="Untitled", body="")
+        i.slug = self.generate_slug(i.title)
+        db.new_post(i.slug, i.title, i.body)
+        raise web.seeother("/" + i.slug)
+
+    def generate_slug(self, title):
+        slug = title.lower().replace(" ", "-")
+        # TODO: if the slug is already present in database, generate new one
+        return slug
 
 if __name__ == "__main__":
     app.run()
