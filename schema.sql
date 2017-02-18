0edc735464583816b34df73e7ca80dafbf6e1621
Anand Chitipothu <anandology@gmail.com>
2017-02-18 10:27:29 +0530

    Added schema.sql

---
+++ schema.sql
@@ -0,0 +1,18 @@
+-- Database schema and sample data for simple blog using web.py
+--
+-- create a database using:
+--
+--    sqlite3 blog.db < schema.sql
+--
+create table post (
+	id integer auto increment primary key,
+	slug text unique,
+	title text,
+	body text,
+	created timestamp default current_timestamp
+);
+
+insert into post (slug, title, body) 
+	values ('hello-world', "Hello World", "First blog post");
+insert into post (slug, title, body) 
+	values ('hello-again', "Hello Hello!", "Another blog post");
